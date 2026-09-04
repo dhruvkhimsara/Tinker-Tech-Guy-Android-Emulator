59c7649b9b9a3aa289e79be0c6a901d79cf0f35b
Tinker Tech Guy <dhruvkhimsara@gmail.com>
2026-09-04 16:57:25 -0700

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-console.log("Tinker Tech Guy Android Emulator loaded!");
+console.log("Tinker Tech Guy Android Emulator starting...");
+
+const script = document.createElement("script");
+script.src = "./qemu/out.js";
+
+script.onload = () => {
+    console.log("QEMU-Wasm JavaScript loaded!");
+};
+
+script.onerror = (error) => {
+    console.error("Failed to load QEMU-Wasm:", error);
+};
+
+document.body.appendChild(script);
