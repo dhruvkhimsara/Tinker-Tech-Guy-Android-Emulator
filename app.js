6346c50d50297da1e8c21880073be62e6edb3d30
Tinker Tech Guy <dhruvkhimsara@gmail.com>
2026-09-05 06:51:33 -0700

    build

--- app.js
+++ app.js
@@ -6,13 +6,33 @@ console.log("QEMU-Wasm module imported!");
 
 const Module = {
     noInitialRun: true,
-    mainScriptUrlOrBlob: new URL("./qemu/qemu-system-aarch64.js", window.location.href).href
+
+    mainScriptUrlOrBlob:
+        new URL("./qemu/qemu-system-aarch64.js", window.location.href).href,
+
+    arguments: [
+        "-machine", "type=ranchu",
+        "-cpu", "cortex-a57",
+        "-m", "4096",
+        "-smp", "4",
+
+        "-kernel", "/qemu/kernel-ranchu",
+        "-initrd", "/qemu/ramdisk.img",
+
+        "-append",
+        "console=ttyAMA0,38400 keep_bootcon earlyprintk=ttyAMA0",
+
+        "-serial", "stdio",
+        "-nographic"
+    ]
 };
 
 (async () => {
     try {
         const instance = await initEmscriptenModule(Module);
+
         console.log("QEMU-Wasm initialized!", instance);
+
     } catch (error) {
         console.error("QEMU-Wasm initialization failed:", error);
     }
